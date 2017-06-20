28bb2855139632851e526c6336fa500e61ebe09d
DIR\sudipta.c.das <sudipta.c.das@accenture.com>
2017-06-20 17:39:13 +0530

    changes

--- server.js
+++ server.js
@@ -13,13 +13,24 @@
 //    response.end('Hello World\n');
 //}).listen(8080);
 
+/**
+ * Created by sudipta.c.das on 6/18/2017.
+ */
+//var http = require("http");
+//var url = require('url');
+//var dt = require('./Authentication/auth');
+//
+//http.createServer(function (request, response) {
+//    response.writeHead(200, {'Content-Type': 'application/json'});
+//    var q = url.parse(request.url, true).query;
+//    response.write("Current time is: " + dt.currentDateAndTime() + "\n");
+//    response.write("Name: " + q.loginName + "\n");
+//    response.end('Hello World\n');
+//}).listen(8080);
+
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app)
-    , io     = require('socket.io').listen(server)
-    , connect = require('connect')
-    , pg     = require('pg')
-    , Client = pg.Client;
+
 
 // Setup express middleware
 app.use(express.static('public'));
@@ -36,32 +47,38 @@ firebase.initializeApp({
 var db = firebase.database();
 
 app.get('/category', function (req, res) {
-    var timer = setTimeout(function () {
-        fetchCategoryFromFirebase(res);
-    }, 1000);
-    req.once('timeout', function () {
-        clearTimeout(timer);
-    });
+    /*var timer = setTimeout(function () {
+     fetchCategoryFromFirebase(res);
+     }, 1000);
+     req.once('timeout', function () {
+     clearTimeout(timer);
+     });*/
+    fetchCategoryFromFirebase(res);
 });
+
+app.use(function(req, res) {
+    res.status(404).send("Not Found");
+})
 
 var fetchCategoryFromFirebase = function (response) {
     var ref = db.ref("category");
-    ref.on("value", function(snapshot) {
+    ref.once("value", function(snapshot) {
         var categoryDataSource = [];
         snapshot.forEach(function (data) {
             categoryDataSource.push(data.val());
         });
-        response.send(categoryDataSource);
-        response.end();
+        console.log(categoryDataSource);
+        response.json({data: categoryDataSource});
+        //response.end();
     }, function (errorObject) {
-        response.send("The read failed: " + errorObject.code);
+        response.status(500).send("The read failed: " + errorObject.code);
     });
 };
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.listen(server_port, server_ip_address, function () {
+app.listen(server_port, function () {
     console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 console.log("Running on port " + server_port + "....");
